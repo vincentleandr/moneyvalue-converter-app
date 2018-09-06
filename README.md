dad250cf4fe17d52ebe2805321d73ab1f9a5063c
vincentleandr <31660411+vincentleandr@users.noreply.github.com>
2018-09-06 16:54:09 +0700

    .

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # moneyvalue-converter-app
+
+To run this app locally:
+1. Clone
+2. npm install
+3. npm start
--- src/App.js
+++ src/App.js
@@ -8,89 +8,98 @@ class App extends Component {
         this.state = {
             input: '',
             showDisp: 'hidden',
-			gotoTop: '',
 			bill: [100000, 50000, 20000, 10000, 5000, 1000, 500, 100, 50],
-			billResult: []
+			billResult: [],
+			errorMsg: ''
         }
 
         this.onInputChange = this.onInputChange.bind(this);
-		this.onSubmitForm = this.onSubmitForm.bind(this);
+		this.onResetForm = this.onResetForm.bind(this);
+		this.validateForm = this.validateForm.bind(this);
     }
 
     onInputChange(event) {
         this.setState({input: event.target.value});
 	}
 
-    onSubmitForm(event) {
-		event.preventDefault();
+	validateForm(event) {
+		event.preventDefault(); // prevent from reload
 
-		//console.log(this.state.input);
-		//console.log(this.state.bill.length) input.substring(0, input.indexOf("-"));
+		let onlyNum = Number(this.state.input.replace(/[^0-9,-]+/g,"").split(",")[0]); // take only number from the input
+		let convertedInt = parseInt(onlyNum, 10); // parse the number value to integer
 
-		//let onlyNum = Number(this.state.input.replace(/[^0-9,-]+/g,"").replace(/,00/g, ''));
+		document.getElementById('label').innerText = 'To match Rp. ' + convertedInt.toLocaleString(['ban', 'id']) + ', you need:'; // pass the input value to label for the visual
 
-		let onlyNum = Number(this.state.input.replace(/[^0-9,-]+/g,"").split(",")[0]);
-		let convertedInt = parseInt(onlyNum, 10);
+		if(this.state.input == null || this.state.input === '') { // empty input
+			this.setState({
+				showDisp: 'hidden',
+				errorMsg: 'Cannot be empty.',
+			}, function () {
+				console.log(this.state.errorMsg);
+			});
+		} else if(convertedInt === 0) { // if no number detected
+			this.setState({
+				showDisp: 'hidden',
+				errorMsg: 'Missing value.',
+			}, function () {
+				console.log(this.state.errorMsg);
+			});
+		} else { // validation passed
 
-		console.log(convertedInt);
+			if(convertedInt < 50 && convertedInt !== 0) { // if initial input is smaller than the smallest bill
+				this.setState({ billResult: this.state.billResult.push("<li>No available fraction for Rp. " + convertedInt + "</li>x") });
+			} else if (convertedInt >= 50) {
+				for(var i = 0; i < this.state.bill.length; i++) { // loop the input value through the bill
 
-		if(convertedInt < 50 && convertedInt !== 0) {
-			this.setState({ billResult: this.state.billResult.push("No available fraction for Rp. " + convertedInt) });
-		}
-
-		for(var i = 0; i < this.state.bill.length; i++) {
-			while(convertedInt >= this.state.bill[i]) {
-				this.setState({ billResult: this.state.billResult.push(this.state.bill[i])});
-				convertedInt = convertedInt - this.state.bill[i];
-
-				if(convertedInt < 50 && convertedInt !== 0) {
-					this.setState({ billResult: this.state.billResult.push("No available fraction for Rp. " + convertedInt) });
+					// when input value is still greater than the bill, push the array of bill to the billResult
+					while(convertedInt >= this.state.bill[i]) {
+						this.setState({ billResult: this.state.billResult.push("<li>Rp. " + this.state.bill[i].toLocaleString(['ban', 'id']) + "</li>x")});
+						convertedInt = convertedInt - this.state.bill[i]; // count the amount left
+		
+						if(convertedInt < 50 && convertedInt !== 0) { // if the amount left is smaller than the smallest bill
+							this.setState({ billResult: this.state.billResult.push("<li>No available fraction for Rp. " + convertedInt + "</li>x") });
+						}
+					}
 				}
 			}
+
+			// count the duplicates in the array
+			let convertedHtml = this.state.billResult;
+			let count = {};
+			convertedHtml.forEach(function(i) {
+				count[i] = (count[i]||0) + 1;
+			});
+
+			console.log(count);
+
+			document.getElementById('list').innerHTML = JSON.stringify(count).replace(/{|}|:|"|,|/g, ""); // push the list
+
+			this.onResetForm();
 		}
+	}
 
-		let bills = this.state.billResult;
-
-		let billsFiltered = bills.filter(function(item) {
-			return typeof item === 'number';
-		});
-
-		//console.log(billsFiltered);
-
-
-		let convertedHtml = billsFiltered.map(bill => {
-			<li>{bill}</li>
-		});
-
-		//console.log(convertedHtml);
-
-		document.getElementById('list').innerHTML = convertedHtml;
-
-		// document.getElementById('list').innerHTML = this.state.billResult.map(billResult => {
-		// 	<li>{billResult}</li>
-		// });
-		
+    onResetForm() {		
         this.setState({
             input: '',
             showDisp: 'show',
-			gotoTop: 'top',
-			billResult: []
+			billResult: [],
+			errorMsg: ''
 		});
-
-		console.log(this.state.billResult);
 	}
 	
 	render() {
 		return (
 			<div className="container">
-				<form className="main-form" onSubmit={this.onSubmitForm}>
-					<input className="main-form__input" type="text" placeholder="Try '15000'" value={this.state.input} onChange={this.onInputChange} />
+				<form className="main-form" onSubmit={this.validateForm}>
+					<input className="main-form__input" type="text" placeholder="Try '15000'" value={this.state.input} onChange={this.onInputChange} autoFocus/>
 					<button className="main-form__btn" type="submit">Go</button>
 				</form>
+				<span className="error-msg">{this.state.errorMsg}</span>
 
-				<ul id="list">
-					{/* {this.state.billResult.map(bill => <li>{bill}</li>)} */}
-				</ul>
+				<div className={"display " + this.state.showDisp}>
+					<span id="label"></span>
+					<ul id="list"></ul>
+				</div>
 			</div>
 		);
 	}
